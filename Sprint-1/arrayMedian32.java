ebd10fcc621a55a134ff6adf80cca91df0178eac
Delta Student <151124074+priyachauh5@users.noreply.github.com>
2024-10-24 22:48:13 +0530

    First commit

--- Sprint-1/arrayMedian32.java
+++ Sprint-1/arrayMedian32.java
@@ -1,15 +1,11 @@
 import java.util.*;
 public class arrayMedian32{
     public static void arrayMedian(int array){
-        // int sort[]=new int[array.length];
-        // int maxvalue=Integer.MIN_VALUE;
-        for(int i=0; i<array.length-1; i++){
-            for(int j=0; j<array.length-1-i; j++){
-                if(array[j]>arr[j+1]){
-                    int temp=array[j];
-                    array[j]=array[j+1];
-                    array[j+1]=temp;
-                }
+        int sort[]=new int[array.length];
+        int maxvalue=Integer.MIN_VALUE;
+        for(int i=0; i<array.length; i++){
+            if(maxvalue<sort[i]){
+                
             }
         }
     }
