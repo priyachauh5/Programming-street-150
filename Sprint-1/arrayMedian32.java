b7c5c91a3659bbea2d5551eb2864b6d421a858c4
Delta Student <151124074+priyachauh5@users.noreply.github.com>
2024-10-25 23:47:01 +0530

    first commit

--- Sprint-1/arrayMedian32.java
+++ Sprint-1/arrayMedian32.java
@@ -1,3 +1,15 @@
+//------------------------------------------------------------------------------------------------------------------
+/*Finding the Median of an Array
+Difficulty: Medium
+Topics: Arrays, Sorting
+Description: Write a program to find the median of an array of numbers.
+Example:
+Input: array = [3, 1, 2, 4, 5]
+Output: 3
+Explanation: The median of the sorted array [1, 2, 3, 4, 5] is 3.*/
+//------------------------------------------------------------------------------------------------------------------
+
+
 import java.util.*;
 public class arrayMedian32{
     public static void arrayMedian(int array){
