533c9426d7d77e0d192f3423ae778fefa7248c1f
Delta Student <151124074+priyachauh5@users.noreply.github.com>
2024-11-04 22:13:25 +0530

    Uploaded successfully

--- Sprint-1/squareMatrix39.java
+++ Sprint-1/squareMatrix39.java
@@ -17,30 +17,6 @@ import java.util.Scanner;
 
 public class modeOfNum45{
 
-        // Method to generate a square matrix filled with sequential numbers
-    // public static int[][] generateMatrix(int size) {
-    //     int[][] matrix = new int[size][size];
-    //     int num = 1;
-        
-    //     for (int i = 0; i < size; i++) {
-    //         for (int j = 0; j < size; j++) {
-    //             matrix[i][j] = num++;
-    //         }
-    //     }
-        
-    //     return matrix;
-    // }
-    // public static void main(String[] args) {
-    //     Scanner scanner = new Scanner(System.in);
-
-    //     System.out.print("Enter the size of the square matrix: ");
-    //     int size = scanner.nextInt();
-        
-    //     int[][] matrix = generateMatrix(size);
-        
-    //     System.out.println("Generated Square Matrix:");
-    //     printMatrix(matrix);
-
         public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
