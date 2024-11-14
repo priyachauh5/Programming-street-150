24f94ae3c372fc0100512c188edc26c5f9d91541
Delta Student <151124074+priyachauh5@users.noreply.github.com>
2024-11-14 23:45:15 +0530

    uploaded successfully

---
+++ Sprint-2/hsbdjbjkd.java
@@ -0,0 +1,10 @@
+//------------------------------------------------------------------------------------------------------------------
+/*Counting the Number of Palindromic Substrings in a String
+Difficulty: Medium
+Topics: String Manipulation
+Description: Write a program to count how many palindromic substrings exist in a given string.
+Example:
+Input: string = "aaa"
+Output: 6
+Explanation: Palindromic substrings are "a", "a", "a", "aa", "aa", "aaa".  */
+//------------------------------------------------------------------------------------------------------------------
