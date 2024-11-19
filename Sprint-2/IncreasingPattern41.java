53430cb18590d0c9ec8c67db64b866d7eb37d78f
Delta Student <151124074+priyachauh5@users.noreply.github.com>
2024-11-19 23:52:49 +0530

    uploaded successfully

---
+++ Sprint-2/IncreasingPattern41.java
@@ -0,0 +1,13 @@
+//------------------------------------------------------------------------------------------------------------------
+/*Generating a Pattern of Increasing Numbers
+Difficulty: Easy
+Topics: Patterns
+Description: Write a program to create a pattern where numbers increase with each row.
+Example:
+Input: rows = 3
+Output:
+
+1  
+12  
+123*/
+//------------------------------------------------------------------------------------------------------------------
