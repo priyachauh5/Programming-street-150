2e367dc0fd342bccd36fb2163adb05ee0a7ad237
Delta Student <151124074+priyachauh5@users.noreply.github.com>
2024-11-30 22:53:45 +0530

    code done

--- Sprint-2/jskdjnja.java
+++ Sprint-2/jskdjnja.java
@@ -1,13 +1,3 @@
 //------------------------------------------------------------------------------------------------------------------
-/*Generating a Pattern of Prime Numbers
-Difficulty: Medium
-Topics: Prime Numbers, Patterns
-Description: Write a program to generate a pattern where each row contains the first few prime numbers.
-Example:
-Input: rows = 3
-Output:
-
-2  
-2 3  
-2 3 5  */
+/* */
 //------------------------------------------------------------------------------------------------------------------
