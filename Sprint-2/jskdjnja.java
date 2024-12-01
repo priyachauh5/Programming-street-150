7061e5c71a9d0d78a429014cfe5b012685d60ea5
Delta Student <151124074+priyachauh5@users.noreply.github.com>
2024-12-01 23:58:03 +0530

    commit initialized

--- Sprint-2/jskdjnja.java
+++ Sprint-2/jskdjnja.java
@@ -1,3 +1,10 @@
 //------------------------------------------------------------------------------------------------------------------
-/* */
+/* Finding the Median of a List of Numbers
+Difficulty: Medium
+Topics: Sorting, Mathematical Computations
+Description: Write a program to find the median value of a list of numbers.
+Example:
+Input: list = [3, 1, 4, 1, 5]
+Output: 3
+Explanation: After sorting the list to [1, 1, 3, 4, 5], the median is 3. */
 //------------------------------------------------------------------------------------------------------------------
