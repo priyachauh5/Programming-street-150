0571dc19f7c6c492d9f5781f48a73f502bdbc6c2
Delta Student <151124074+priyachauh5@users.noreply.github.com>
2024-12-02 00:20:56 +0530

    commit initialized

--- Sprint-2/median.java
+++ Sprint-2/median.java
@@ -8,3 +8,21 @@ Input: list = [3, 1, 4, 1, 5]
 Output: 3
 Explanation: After sorting the list to [1, 1, 3, 4, 5], the median is 3. */
 //------------------------------------------------------------------------------------------------------------------
+
+import java.util.Arrays;
+public class median{
+    public static void arrayMedian(int arr[]){
+        Arrays.sort(arr);
+        // System.out.print(Arrays.toString(arr));
+        int n=arr.length;
+        if(n%2==1){
+            System.out.print("Median of the array is: "+arr[n/2]);
+        }
+    }
+    public static void main(String args[]){
+        int arr[]={3,1,4,1,5};
+        arrayMedian(arr);
+    }
+}
+
+
